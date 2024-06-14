c79e7882ecc68d80fbcce97ab064d5025d5259f6
Maicol Moet <PS250444@summacollege.nl>
2024-06-14 14:23:58 +0200

    Created img file, added sprites, updated config file for database connection.

---
+++ Eindopdracht/classes/dbHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace PHP_WPF_Eindopdracht
+{
+    internal class dbHandler
+    {
+
+    } 
+}
